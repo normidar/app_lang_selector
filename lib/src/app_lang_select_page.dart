711855db4c90d6093740c3509073ac55ce69a3ee
ryu tatsu <xtatsu_ryu@serio.inc>
2025-08-25 16:24:58 +0900

    Remove unused auto_exporter_annotation dependency and delete utils.dart file, cleaning up imports in app_lang_select_page.dart and app_lang_select_tile.dart.

--- lib/src/app_lang_select_page.dart
+++ lib/src/app_lang_select_page.dart
@@ -1,5 +1,4 @@
 import 'package:app_lang_selector/src/selecting_lang/selecting_lang.dart';
-import 'package:app_lang_selector/src/utils.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
--- lib/src/utils.dart
+++
@@ -1,4 +0,0 @@
-import 'package:auto_exporter_annotation/auto_exporter_annotation.dart';
-
-@IgnoreExport()
-const pkgName = 'app_lang_selector';
