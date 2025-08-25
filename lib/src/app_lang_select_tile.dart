7404df4d81b6c9bedbdeb2e6080f2f7187525e1a
normidar <normidar7@gmail.com>
2025-08-25 23:07:25 +0900

    Refactor AppLangSelectTile to load localization JSON directly and improve language selection functionality.

--- lib/src/app_lang_select_tile.dart
+++ lib/src/app_lang_select_tile.dart
@@ -1,6 +1,7 @@
 import 'package:app_lang_selector/app_lang_selector.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AppLangSelectTile extends ConsumerWidget {
@@ -9,29 +10,24 @@ class AppLangSelectTile extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final _ = ref.watch(selectingLangProvider);
-    return EasyLocalization(
-      supportedLocales: const [
-        Locale('ja', 'JP'),
-        Locale('en', 'US'),
-        Locale('zh', 'CN'),
-        Locale('tr', 'TR'),
-        Locale('pt', 'PT'),
-        Locale('es', 'ES'),
-      ],
-      path: 'packages/app_lang_selector/assets/localizations',
-      fallbackLocale: const Locale('en', 'US'),
-      child: ListTile(
-        leading: const Icon(Icons.language),
-        title: Text('select_lang'.tr()),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute<void>(
-              builder: (context) => const AppLangSelectPage(),
-            ),
-          );
-        },
-      ),
+
+    () async {
+      final json = await rootBundle.loadString(
+          'packages/app_lang_selector/assets/localizations/en-US.json');
+      print('json:::');
+      print(json);
+    }();
+    return ListTile(
+      leading: const Icon(Icons.language),
+      title: Text('select_lang'.tr()),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute<void>(
+            builder: (context) => const AppLangSelectPage(),
+          ),
+        );
+      },
     );
   }
 }
